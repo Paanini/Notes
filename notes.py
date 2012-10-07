3b9b579c5cf233047acbd982fee44ef27a447cab
Paanini Navilekar <paanini16@gmail.com>
2012-10-07 21:08:16 +0530

    File I/O working

--- notes.py
+++ notes.py
@@ -6,14 +6,14 @@ conn = sqlite3.connect('/media/Software/Dropbox/SimplyNotes/inotes.bak')
 c = conn.cursor()
 notes = c.execute(
     'SELECT text, created, gtask_completed, _id FROM notes').fetchall()
-
+titles = []
 for note in notes:
-#    print note[0], type(note[0])
-#    print note[1], type(note[1])
     try:
         title = re.search(r'.+?\n', note[0]).group(0)[:-1]
+        titles.append(title)
     except AttributeError:
         title = re.search(r'\w+', note[0]).group(0)
+        titles.append(title)
     f = open(path + title, "w")
     text = str(note[0]) + "\n\nDate Created: " + str(note[1]) + "\n\nDate Completed: " + str(note[2])
     f.write(text)
