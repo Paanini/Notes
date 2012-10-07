aa8c4679695fc7c01471e4002ffba91162037ff5
Paanini Navilekar <paanini16@gmail.com>
2012-10-07 21:01:27 +0530

    File I/O working

--- notes.py
+++ notes.py
@@ -6,27 +6,14 @@ conn = sqlite3.connect('/media/Software/Dropbox/SimplyNotes/inotes.bak')
 c = conn.cursor()
 notes = c.execute(
     'SELECT text, created, gtask_completed, _id FROM notes').fetchall()
-text = []
-date_create = []
-date_done = []
-#for note in notes:
-#    try:
-#        title=re.search(r'.+?\n',note[0]).group(0)[:-1]
-#    except AttributeError:
-#        title=re.search(r'\w+',note[0]).group(0)
-#    f=open(path+title,"w")
-#    f.write(note[0]+"\n\n Date of Creation: "+note[1]+"\n\n Date of Completion: "+note[2])
-#    f.close()
-#    text.append(note[0])
-#    date_create.append(note[1])
-#    date_create.append(note[2])
-#print notes[2]
-for note in notes:
-    print note[0]
-    print "\n"
-    print "*" * 20
-    print note[1]
 
-print text
-print date_create
-print date_done
+for note in notes:
+#    print note[0], type(note[0])
+#    print note[1], type(note[1])
+    try:
+        title = re.search(r'.+?\n', note[0]).group(0)[:-1]
+    except AttributeError:
+        title = re.search(r'\w+', note[0]).group(0)
+    f = open(path + title, "w")
+    text = str(note[0]) + "\n\nDate Created: " + str(note[1]) + "\n\nDate Completed: " + str(note[2])
+    f.write(text)
